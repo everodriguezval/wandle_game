1e382edcdddbfd0cc8dfd68c03d35ac690d9a78b
Oliver Cadman <o.cadman@live.co.uk>
2023-03-29 20:58:21 +0100

    Added starter code

---
+++ wordle/assets/js/starter.js
@@ -0,0 +1,62 @@
+// Grab DOM elements
+
+async function init() {
+    /**
+     * Define following variables:
+     * - currentGuess
+     * - currentRow
+     * - answerLength
+     * - done
+     */
+
+    /**
+     * Make API call, get word of the day.
+     * Create array of characters
+     */
+
+
+    function addLetter(letter) {
+        // check if buffer is less than 5 characters
+            // if so, add letter
+            // if not, replace last letter with new letter
+
+    }
+
+    function handleCommit(){
+        // If word doesn't contain 5 letters...
+
+
+
+        // Mark 'correct', 'close', 'wrong' squares
+
+        // Did the user win or lose?
+
+
+        // set currentGuess to empty string
+        // increment currentRow
+    }
+
+    function handleBackspace() {
+        // Modify currentGuess and update DOM
+    }
+
+
+    /**
+     * Listen for keystrokes and perform actions based on the following:
+     * 
+     * - is the key Enter
+     * - is the key Backspace
+     * - is the key a valid letter
+     * - otherwise...
+     */
+}
+
+function isLetter(action) {
+    // Check if keystroke is indeed a letter
+}
+
+function makeMap(array) {
+    // Create object of 
+}
+
+init();
